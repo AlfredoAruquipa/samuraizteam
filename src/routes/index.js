e2e13f3b7e163820248566c6e3daa9b61d1a61d2
AlfredoAruquipa <leebyungchulk@gmail.com>
2024-02-25 18:02:46 -0400

    ventanasModificadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 const router = Router()
 
-router.get("/", (req, res) => res.render("index", { title: "primera pagina con node"}))
-router.get("/about", (req, res) => res.render("about", { title: "about me", x:30}))
-router.get("/contact", (req, res) => res.render("contact", { title: "contact me"}))
-router.get("/integrant", (req, res) => res.render("integrant", { title: "contact me"}))
-export default router
+router.get("/", (req, res) => res.render("index", { title: "Inicio"}))
+router.get("/about", (req, res) => res.render("about", { title: "Sobre Nosotros"}))
+router.get("/contact", (req, res) => res.render("contact", { title: "Contactanos"}))
+router.get("/integrant", (req, res) => res.render("integrant", { title: "Integrantes del Team"}))
+router.get("/events", (req, res) => res.render("events", { title: "Eventos"}))
+router.get("/store", (req, res) => res.render("store", { title: "Nuestra Tienda"}))
+export default router 
